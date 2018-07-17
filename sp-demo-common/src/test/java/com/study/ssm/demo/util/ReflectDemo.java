5876c48c916ed7ca62c7003e9880047c9af7fe67
v_yuanjiajia <v_yuanjiajia@saicmotor.com>
2018-07-17 09:21:46 +0800

    修改文件夹位置

--- sp-demo-common/src/test/java/com/study/ssm/demo/util/ReflectDemo.java
+++ sp-demo-common/src/test/java/com/study/ssm/demo/util/ReflectDemo.java
@@ -19,17 +19,17 @@ public class ReflectDemo {
 	public static void reflect01() throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, SecurityException, InstantiationException, InvocationTargetException{
 		 Student student = new Student();
 		 student.setId(122L);
-		 student.setAddress("上海");
+		 student.setAddress("???");
 		 student.setMobilePhone("12345");
 		 student.setPassword("23332");
-		 student.setUserName("姓名");
+		 student.setUserName("????");
 		 Class<? extends Student> class1 = student.getClass();
 		 Field[] declaredFields = class1.getDeclaredFields();
 		 for (Field field : declaredFields) {
 			 String name = field.getName();
 			 field.setAccessible(true);
 			 Object object = field.get(student);
-			 System.out.println("属性名="+name+"===="+"属性值="+object);
+			 System.out.println("??????="+name+"===="+"?????="+object);
 			 System.out.println("============");
 		 }
 		 Constructor<? extends Student> constructor = class1.getConstructor(null);
@@ -40,18 +40,18 @@ public class ReflectDemo {
 	public static void reflect02() throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		 Student student = new Student();
 		 student.setId(122L);
-		 student.setAddress("北京");
+		 student.setAddress("????");
 		 student.setMobilePhone("12345");
 		 student.setPassword("23332");
-		 student.setUserName("姓名");
+		 student.setUserName("????");
 		 BeanInfo beanInfo = Introspector.getBeanInfo(student.getClass());
 		 PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
 		 for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-			 String name = propertyDescriptor.getName();//属性名
+			 String name = propertyDescriptor.getName();//??????
 			 Method readMethod = propertyDescriptor.getReadMethod();
 			 Object invoke = readMethod.invoke(student);
 			 String string = invoke.toString();
-			 System.out.println("属性名="+name+"===="+"属性值="+string);
+			 System.out.println("??????="+name+"===="+"?????="+string);
 			 System.out.println("============");
 		}
 	}
